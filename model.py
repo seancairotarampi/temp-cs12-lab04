7016eea4851cdc66aa4ff7cb81294d83778c5ef6
2025-03125 <deramos3@up.edu.ph>
2026-02-27 12:41:50 +0800

    update

--- model.py
+++ model.py
@@ -2,14 +2,40 @@
 from common_types import Player, WinConditionType, TokenPhysicsType
 
 class ConnectTacToeModel:
-    def __init__(self, p1: Player, p2: Player, win_condition: WinConditionType, token_physics: TokenPhysicsType) -> None:
-        ...
+    def __init__(self, win_condition: WinConditionType, token_physics: TokenPhysicsType) -> None:
+        self._current_player = Player.P1
+        self._win_condition = win_condition
+        self._token_physics = token_physics
+        self._grid = [
+                    [".", ".", ".", ".", ".", ".", "."],
+                    [".", ".", ".", ".", ".", ".", "."],
+                    [".", ".", ".", ".", ".", ".", "."],
+                    [".", ".", ".", ".", ".", ".", "."],
+                    [".", ".", ".", ".", ".", ".", "."],
+                    [".", ".", ".", ".", ".", ".", "."]
+                    ]
 
     def choose_cell(self, row: int, col: int) -> bool:
-        ...
+        if self._grid[row][col] != ".":
+            return False
+        else:
+            if self._current_player == Player.P1:
+                self._grid[row][col] = "A"
+                self._current_player = Player.P2
+                return True
+            else:
+                self._grid[row][col] = "B"
+                self._current_player = Player.P1
+                return True
 
     def get_owner(self, row: int, col: int) -> Player | None:
-        ...
+        if self._grid[row][col] == "A":
+            return Player.P1
+        elif self._grid[row][col] == "B":
+            return Player.P2
+        else:
+            return None
+        
 
     @property
     def current_player(self) -> Player:
