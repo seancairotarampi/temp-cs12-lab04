8498f08aa8fc5f9f3ceefaf12d36d57c4953a563
seancairotarampi <seancairotarampi@gmail.com>
2026-02-27 12:42:40 +0800

    add to main, view

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
 from argparse import ArgumentParser
 from model import ConnectTacToeModel
+from view import ConnectTacToeView, ConnectTacToeController
+from common_types import WinConditionType, TokenPhysicsType
 
+parser: ArgumentParser = ArgumentParser()
+parser.add_argument("-p", "--token_physics", default="floating")
+parser.add_argument("-w", "--win_condition", default="tictactoe")
+args = parser.parse_args()
+
+def main() -> None:
+    tp: TokenPhysicsType
+    wc: WinConditionType
+    if args.token_physics == "weak":
+        tp = TokenPhysicsType.WEAK_GRAVITY
+    elif args.token_physics == "strong":
+        tp = TokenPhysicsType.STRONG_GRAVITY
+    else:
+        tp = TokenPhysicsType.FLOATING
+    if args.win_condition == "notconnectfour":
+        wc = WinConditionType.NOT_CONNECT_FOUR
+    else:
+        wc = WinConditionType.TIC_TAC_TOE
+
+    m: ConnectTacToeModel = ConnectTacToeModel(wc, tp)
+    v: ConnectTacToeView = ConnectTacToeView()
+    c: ConnectTacToeController = ConnectTacToeController(m, v)
+
+if __name__ == "__main__":
+    main()
--- view.py
+++ view.py
@@ -1,4 +1,13 @@
 # pyright: strict
+from model import ConnectTacToeModel
 
 class ConnectTacToeView:
-    def display_view()
+    def display_view(self):
+        ...
+
+class ConnectTacToeController:
+    def __init__(self, model: ConnectTacToeModel, view: ConnectTacToeView) -> None:
+        pass
+    
+    def run(self) -> None:
+        ...
