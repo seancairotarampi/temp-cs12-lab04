9ef265608c059c292467c214f1b88419c0a9815e
2025-03125 <deramos3@up.edu.ph>
2026-02-27 12:11:49 +0800

    Test

--- model.py
+++ model.py
@@ -1,4 +1,25 @@
 # pyright: strict
 
 class ConnectTacToeModel:
-    ...
+    def __init__(self, win_condition: WinConditionType, token_physics: TokenPhysicsType):
+        ...
+
+    @property
+    def current_player(self) -> Player:
+        ...
+
+    @property
+    def winner(self) -> Player | None:
+        ...
+
+    @property
+    def is_game_done(self) -> bool:
+        ...
+
+    @property
+    def row_count(self) -> int:
+        ...
+
+    @property
+    def col_count(self) -> int:
+        ...
